a054f35807f296da8a229c91e1eb26103d5db33d
Lori He <44847418+lorihe@users.noreply.github.com>
2023-11-06 14:50:52 -0500

    upload

---
+++ SQL_practice/SQL Tasks Tier 2.sql
@@ -0,0 +1,229 @@
+/* Welcome to the SQL mini project. You will carry out this project partly in
+the PHPMyAdmin interface, and partly in Jupyter via a Python connection.
+
+This is Tier 2 of the case study, which means that there'll be less guidance for you about how to setup
+your local SQLite connection in PART 2 of the case study. This will make the case study more challenging for you: 
+you might need to do some digging, aand revise the Working with Relational Databases in Python chapter in the previous resource.
+
+Otherwise, the questions in the case study are exactly the same as with Tier 1. 
+
+PART 1: PHPMyAdmin
+You will complete questions 1-9 below in the PHPMyAdmin interface. 
+Log in by pasting the following URL into your browser, and
+using the following Username and Password:
+
+URL: https://sql.springboard.com/
+Username: student
+Password: learn_sql@springboard
+
+The data you need is in the "country_club" database. This database
+contains 3 tables:
+    i) the "Bookings" table,
+    ii) the "Facilities" table, and
+    iii) the "Members" table.
+
+In this case study, you'll be asked a series of questions. You can
+solve them using the platform, but for the final deliverable,
+paste the code for each solution into this script, and upload it
+to your GitHub.
+
+Before starting with the questions, feel free to take your time,
+exploring the data, and getting acquainted with the 3 tables. */
+
+
+/* QUESTIONS 
+/* Q1: Some of the facilities charge a fee to members, but some do not.
+Write a SQL query to produce a list of the names of the facilities that do. */
+
+SELECT name
+FROM Facilities
+WHERE membercost >0
+
+
+/* Q2: How many facilities do not charge a fee to members?
+
+SELECT COUNT( * ) 
+FROM Facilities
+WHERE membercost = 0
+
+/* A2: 4 */
+
+
+/* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
+where the fee is less than 20% of the facility's monthly maintenance cost.
+Return the facid, facility name, member cost, and monthly maintenance of the
+facilities in question. */
+
+SELECT facid, name, membercost, monthlymaintenance
+FROM `Facilities`
+WHERE membercost < monthlymaintenance * 0.2 
+
+
+/* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
+Try writing the query without using the OR operator. */
+
+SELECT * 
+FROM Facilities
+WHERE facid IN (1, 5)
+
+
+/* Q5: Produce a list of facilities, with each labelled as
+'cheap' or 'expensive', depending on if their monthly maintenance cost is
+more than $100. Return the name and monthly maintenance of the facilities
+in question. */
+
+SELECT name, monthlymaintenance, 
+CASE WHEN monthlymaintenance > 100 
+THEN 'expensive' ELSE 'cheap' 
+END AS label
+FROM Facilities
+
+/* Q6: You'd like to get the first and last name of the last member(s)
+who signed up. Try not to use the LIMIT clause for your solution. */
+
+SELECT surname, firstname
+FROM `Members`
+WHERE joindate = (
+SELECT MAX( joindate )
+FROM `Members` )
+
+/* Q7: Produce a list of all members who have used a tennis court.
+Include in your output the name of the court, and the name of the member
+formatted as a single column. Ensure no duplicate data, and order by
+the member name. */
+
+SELECT DISTINCT CONCAT( m.firstname, ' ', m.surname ) AS member, f.name AS facility
+FROM `Bookings` AS b
+LEFT JOIN `Facilities` AS f ON b.facid = f.facid
+LEFT JOIN `Members` AS m ON b.memid = m.memid
+WHERE f.name LIKE 'Tennis%'
+ORDER BY member
+
+/* Q8: Produce a list of bookings on the day of 2012-09-14 which
+will cost the member (or guest) more than $30. Remember that guests have
+different costs to members (the listed costs are per half-hour 'slot'), and
+the guest user's ID is always 0. Include in your output the name of the
+facility, the name of the member formatted as a single column, and the cost.
+Order by descending cost, and do not use any subqueries. */
+
+SELECT DISTINCT CONCAT( m.firstname, ' ', m.surname ) AS member, f.name AS facility,
+CASE WHEN b.memid =0
+THEN f.guestcost * b.slots
+ELSE f.membercost * b.slots
+END AS cost
+FROM `Bookings` AS b
+LEFT JOIN `Facilities` AS f ON b.facid = f.facid
+LEFT JOIN `Members` AS m ON b.memid = m.memid
+WHERE b.starttime LIKE '2012-09-14%'
+and (((b.memid =0) AND (f.guestcost * b.slots >30))
+OR ((b.memid !=0) AND (f.membercost * b.slots >30)))
+ORDER BY cost DESC
+
+/* Q9: This time, produce the same result as in Q8, but using a subquery. */
+
+SELECT * FROM
+(SELECT DISTINCT CONCAT( m.firstname, ' ', m.surname ) AS member, f.name AS facility,
+CASE WHEN b.memid =0
+THEN f.guestcost * b.slots
+ELSE f.membercost * b.slots
+END AS cost
+FROM `Bookings` AS b
+LEFT JOIN `Facilities` AS f ON b.facid = f.facid
+LEFT JOIN `Members` AS m ON b.memid = m.memid
+WHERE b.starttime LIKE '2012-09-14%')sub 
+WHERE sub.cost >30
+ORDER BY cost DESC
+
+/* PART 2: SQLite
+
+Export the country club data from PHPMyAdmin, and connect to a local SQLite instance from Jupyter notebook 
+for the following questions.  
+
+QUESTIONS:
+/* Q10: Produce a list of facilities with a total revenue less than 1000.
+The output of facility name and total revenue, sorted by revenue. Remember
+that there's a different cost for guests and members! */
+
+/* Table Tennis, Snooker Table, Pool Table */
+
+/* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
+
+/*      members	        recommender
+19	AnnaMackenzie	Darren Smith
+12	AnneBaker	Ponder Stibbons
+6	BurtonTracy	None
+10	CharlesOwen	Darren Smith
+30	DarrenSmith	None
+1	DarrenSmith	None
+24	DavidFarrell	None
+11	DavidJones	Janice Joplette
+17	DavidPinker	Jemima Farrell
+22	DouglasJones	David Jones
+29	EricaCrumpet	Tracy Smith
+15	FlorenceBader	Ponder Stibbons
+0	GUESTGUEST	None
+5	GeraldButters	Darren Smith
+23	HenriettaRumney	Matthew Genting
+25	HenryWorthington-Smyth	Tracy Smith
+27	HyacinthTupperware	None
+14	JackSmith	Darren Smith
+4	JaniceJoplette	Darren Smith
+13	JemimaFarrell	None
+20	JoanCoplin	Timothy Baker
+28	JohnHunt	Millicent Purview
+18	MatthewGenting	Gerald Butters
+26	MillicentPurview	Tracy Smith
+7	NancyDare	Janice Joplette
+9	PonderStibbons	Burton Tracy
+21	RamnareshSarwin	Florence Bader
+8	TimBoothe	Tim Rownam
+3	TimRownam	None
+16	TimothyBaker	Jemima Farrell
+2	TracySmith	None
+*/
+
+/* Q12: Find the facilities with their usage by member, but not guests */
+
+/*
+0     Tennis Court 1
+1     Tennis Court 2
+2    Badminton Court
+3       Table Tennis
+4     Massage Room 1
+5     Massage Room 2
+6       Squash Court
+7      Snooker Table
+8         Pool Table
+*/
+
+
+/* Q13: Find the facilities usage by month, but not guests */
+facid  name             year_month
+0      Tennis Court 1   2012-07       201
+                        2012-08       339
+                        2012-09       417
+1      Tennis Court 2   2012-07       123
+                        2012-08       345
+                        2012-09       414
+2      Badminton Court  2012-07       165
+                        2012-08       414
+                        2012-09       507
+3      Table Tennis     2012-07        98
+                        2012-08       296
+                        2012-09       400
+4      Massage Room 1   2012-07       166
+                        2012-08       316
+                        2012-09       402
+5      Massage Room 2   2012-07         8
+                        2012-08        18
+                        2012-09        28
+6      Squash Court     2012-07        50
+                        2012-08       184
+                        2012-09       184
+7      Snooker Table    2012-07       140
+                        2012-08       316
+                        2012-09       404
+8      Pool Table       2012-07       110
+                        2012-08       303
+                        2012-09       443
+Name: slots, dtype: int64
